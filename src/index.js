6d1db105bc2e0e8cb35a99ca5fbb5dcce750d605
Daniel Yim <daniel.yim96@gmail.com>
2022-11-26 13:56:04 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+  const form = document.getElementById("create-task-form")
+  const text = document.getElementById("new-task-description")
+  const ul = document.createElement("tasks")
+  const div = document.getElementById("list")
+// const submit = document.querySelector(“”)
+document.body.append(ul)
+
+  form.addEventListener("submit", (e)=>{
+    e.preventDefault()
+    
+    div.append(ul)
+    
+    const li = document.createElement("li")
+    const btn = document.createElement("button")
+    ul.append(li)
+    btn.innerText = "X"
+    li.innerText = text.value + " " + btn.value
+    li.append(btn)
+  })
+  form.addEventListener("submit", () => {
+    text.value = " "
+  })
+})
